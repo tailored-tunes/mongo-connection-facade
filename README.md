9667390279888aede8c8b59123bddca9f87a26db
meza <meza@meza.hu>
2014-12-16 21:56:40 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+mongo-connection-facade
+========================
+
+A small facade to make using mongo connections a bit easier
+
+# Installation
+
+Install via composer. Installation help and versions at [Packagist](https://packagist.org/packages/tailored-tunes/mongo-connection-facade)
+
---
+++ src/TailoredTunes/MongoDbConnection.php
@@ -0,0 +1,99 @@
+<?php
+namespace TailoredTunes;
+
+use Mongo;
+use MongoClient;
+
+class MongoDbConnection
+{
+
+    /**
+     * @var String the hostname of the database
+     */
+    private $host;
+    /**
+     * @var String the database username
+     */
+    private $username;
+    /**
+     * @var String the database password
+     */
+    private $password;
+    /**
+     * @var String the database db
+     */
+    private $db;
+    /**
+     * @var Integer the port the db runs on
+     */
+    private $port;
+
+    /**
+     * @var Mongo db connection
+     */
+    private $connection;
+
+    /**
+     * @param String  $host     The hostname the database operates on
+     * @param String  $username The username used to connect to the database
+     * @param String  $password The password used to connect to the database
+     * @param String  $db       The database
+     * @param Integer $port     The port the db listens on
+     */
+    public function __construct($host, $db, $username = "", $password = "", $port = 43047)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->db = $db;
+        $this->port = $port;
+    }
+
+    public function __get($name)
+    {
+        return $this->connection()->$name;
+    }
+
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->connection(), $name], $arguments);
+    }
+
+    /**
+     * Forms the connection String
+     * @return String
+     */
+    public function connectionString()
+    {
+        if (empty($this->username) || empty($this->password)) {
+            return sprintf(
+                "mongodb://%s:%d/%s",
+                $this->host,
+                $this->port,
+                $this->db
+            );
+        }
+        return sprintf(
+            "mongodb://%s:%s@%s:%d/%s",
+            $this->username,
+            $this->password,
+            $this->host,
+            $this->port,
+            $this->db
+        );
+    }
+
+    /**
+     * Gets the connection
+     * @return MongoClient
+     */
+    public function connection()
+    {
+        if (empty($this->connection)) {
+
+            $x = new MongoClient($this->connectionString());
+            $this->connection = $x->selectDB($this->db);
+        }
+        return $this->connection;
+    }
+}
